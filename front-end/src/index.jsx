7920f7b121a689ef231e0f815395e721603db1c6
LowercaseGuy <48055178+VenuGopal1899@users.noreply.github.com>
2020-12-05 21:20:51 +0530

    css styling and fake backend added

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 
 import { store } from './_helpers';
 import { App } from './App';
+import './styles.css';
 
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
